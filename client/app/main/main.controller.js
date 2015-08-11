36f5062787f0eb66f619dfdbaa462190a1816a60
caseywoolley <caseywoolley@gmail.com>
2015-08-11 10:14:49 +0000

    fixed mongod crash

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -19,4 +19,45 @@ angular.module('workspaceApp')
     $scope.deleteThing = function(thing) {
       $http.delete('/api/things/' + thing._id);
     };
+    
+  /*
+    //get yelp json
+    
+    
+
+  var terms = 'food';
+  var near = 'San+Francisco';
+
+  var accessor = {
+    consumerSecret: auth.consumerSecret,
+    tokenSecret: auth.accessTokenSecret
+  };
+
+  var parameters = [];
+  parameters.push(['term', terms]);
+  parameters.push(['location', near]);
+  parameters.push(['oauth_consumer_key', auth.consumerKey]);
+  parameters.push(['oauth_consumer_secret', auth.consumerSecret]);
+  parameters.push(['oauth_token', auth.accessToken]);
+
+  var message = { 
+    'action': 'http://api.yelp.com/v2/search',
+    'method': 'GET',
+    'parameters': parameters 
+  };
+
+  OAuth.setTimestampAndNonce(message);  
+
+  OAuth.SignatureMethod.sign(message, accessor);
+
+  var parameterMap = OAuth.getParameterMap(message.parameters);
+  parameterMap.oauth_signature = OAuth.percentEncode(parameterMap.oauth_signature)
+
+  var url = OAuth.addToURL(message.action,parameterMap);
+  OAuth.get(url, function(data){
+    console.log(data);
+  });
+  //var response = UrlFetchApp.fetch(url).getContentText();
+  //var responseObject = Utilities.jsonParse(response);
+  */
   });
