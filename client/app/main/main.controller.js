acd013381e8935d560f5742a1a38a44f8e4c91b2
caseywoolley <caseywoolley@gmail.com>
2015-08-14 20:10:26 +0000

    added node-yelp

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -3,7 +3,9 @@
 angular.module('workspaceApp')
   .controller('MainCtrl', function ($scope, $http) {
     $scope.awesomeThings = [];
-
+    
+    
+    
     $http.get('/api/things').success(function(awesomeThings) {
       $scope.awesomeThings = awesomeThings;
     });
